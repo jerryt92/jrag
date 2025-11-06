033fc6b84ceb0fab0364b04da0c041e318182872
jerryt92 <jerrytian92@outlook.com>
2025-11-06 14:28:33 +0800

    feat(deployment): 添加Jrag服务的启动、停止和日志查看脚本
    - 添加启动脚本 start_jrag.sh，指定Java环境和配置文件
    - 添加停止脚本 stop_jrag.sh，用于停止Jrag服务
    - 添加日志查看脚本 show_jrag_log.sh，实时查看服务日志

---
+++ deployment/bin/start_jrag.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/ubuntu/jrag/jrag/bin/start_for_linux.sh --java_home /home/ubuntu/jre21 --spring.config.location=classpath:/application-product.yaml,classpath:/application-sqlite.yaml
---
+++ deployment/bin/stop_jrag.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/ubuntu/jrag/jrag/bin/stop_for_linux.sh
